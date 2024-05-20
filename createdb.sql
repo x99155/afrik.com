c76f67ab3be22221a4a4dc59181ef717d85e07d2
x99155 <henrinyemeck@gmail.com>
2024-05-20 13:42:31 +0200

    amélioration du scraper, ajout du gitignore et ajout de fichier de config

--- createdb.sql
+++ createdb.sql
@@ -5,6 +5,8 @@ CREATE DATABASE afrikdb;
 CREATE TABLE articles (
     id SERIAL PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
+    description TEXT NOT NULL,
+    image_url VARCHAR(255) NOT NULL,
     link TEXT NOT NULL,
     date TIMESTAMP NOT NULL
 );
--- scraper/scraper.py
+++ scraper/scraper.py
@@ -2,37 +2,64 @@
 import requests
 from bs4 import BeautifulSoup
 import psycopg2
-from psycopg2.extras import execute_values
 import datetime
+import config
+
 
 def scrape_afrik():
     url = 'https://www.afrik.com/'
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
 
-    articles = []
-    for article in soup.find_all('article'):
-        title = article.find('h2').text.strip()
-        link = article.find('a')['href']
-        date = datetime.datetime.now()
-        
-        articles.append((title, link, date))
-    
-    return articles
+    # Trouver tous les articles
+    articles = soup.find_all('div', {'class': 'td_module_mob_1 td_module_wrap td-animation-stack td-meta-info-hide'})
 
-def save_to_db(articles):
-    conn = psycopg2.connect(
-        dbname="afrikdb", 
-        user="postgres", 
-        password="juana", 
-        host="localhost"
-    )
-    cursor = conn.cursor()
-    execute_values(cursor, "INSERT INTO articles (title, link, date) VALUES %s", articles)
-    conn.commit()
-    cursor.close()
-    conn.close()
+    # Initialiser un dictionnaire pour stocker les informations de l'article
+    article_info = {}
+
+    # Extraire les informations du dernier article publié s'il existe
+    if articles:
+        latest_article = articles[0]
+        link = latest_article.find('a', class_='td-image-wrap')['href']
+        image_url = latest_article.find('img', class_='entry-thumb')['src']
+        title = latest_article.find('h1', class_='entry-title td-module-title').get_text(strip=True)
+        description = latest_article.find('div', class_='td-excerpt').get_text(strip=True)
+        
+        # Stocker ces informations dans le dictionnaire
+        article_info = {
+            'link': link,
+            'image_url': image_url,
+            'title': title,
+            'description': description,
+            'date': datetime.datetime.now()
+        }
+    else:
+        print("Aucun article trouvé")
+    
+    return article_info
+
+def save_to_db(article):
+    try:
+        # Connexion à la base de données PostgreSQL en utilisant les informations de configuration
+        conn = psycopg2.connect(**config.load_config)
+        cursor = conn.cursor()
+
+        # Insertion de l'article dans la base de données
+        cursor.execute("INSERT INTO articles (title, link, description, image_url, date) VALUES (%s, %s, %s, %s, %s)", 
+                       (article['title'], article['link'], article['description'], article['image_url'], article['date']))
+
+        # Validation des modifications et fermeture de la connexion
+        conn.commit()
+        cursor.close()
+        conn.close()
+        print("L'article a été enregistré avec succès dans la base de données.")
+    except (Exception, psycopg2.DatabaseError) as error:
+        print("Erreur lors de l'enregistrement de l'article dans la base de données:", error)
+
 
 if __name__ == "__main__":
-    articles = scrape_afrik()
-    save_to_db(articles)
+    article = scrape_afrik()
+    if article:
+        save_to_db(article)
+    else:
+        print("Aucun article trouvé, aucun enregistrement n'a été effectué.")
